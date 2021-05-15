11253b6cb4ce86bfba012a1a78e732ff9addc772
Hitesh Mishra <hiteshmishra708@gmail.com>
2021-05-15 22:59:51 +0530

    Added programs for casting

---
+++ casting/floats.py
@@ -0,0 +1,15 @@
+/**
+ * @author Hitesh Mishra
+ * @email HiteshMishra708@gmail.com
+ * @create date 2021-05-15 22:58:49
+ * @modify date 2021-05-15 22:58:49
+ * @desc [description]
+ */
+x = float(1)
+y = float(2.8)
+z = float("3")
+w = float("4.2")
+print(x)
+print(y)
+print(z)
+print(w)
---
+++ casting/integers.py
@@ -0,0 +1,13 @@
+/**
+ * @author Hitesh Mishra
+ * @email HiteshMishra708@gmail.com
+ * @create date 2021-05-15 22:58:55
+ * @modify date 2021-05-15 22:58:55
+ * @desc [description]
+ */
+x = int(1)
+y = int(2.8)
+z = int("3")
+print(x)
+print(y)
+print(z)
