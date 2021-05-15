b7e8af9732e0c1b0ae2242a9c9a981302ddd4209
Hitesh Mishra <hiteshmishra708@gmail.com>
2021-05-15 23:05:03 +0530

    Programs for while

---
+++ while_loop/while_break.py
@@ -0,0 +1,13 @@
+/**
+ * @author Hitesh Mishra
+ * @email HiteshMishra708@gmail.com
+ * @create date 2021-05-15 23:04:18
+ * @modify date 2021-05-15 23:04:18
+ * @desc [description]
+ */
+i = 1
+while i < 6:
+  print(i)
+  if (i == 3):
+    break
+  i += 1
