e38e49ae689309f650f74bf9d7dd87c3dfca1852
Hitesh Mishra <hiteshmishra708@gmail.com>
2021-05-15 23:02:51 +0530

    Added programs for numbers

---
+++ numbers/numbers.py
@@ -0,0 +1,14 @@
+/**
+ * @author Hitesh Mishra
+ * @email HiteshMishra708@gmail.com
+ * @create date 2021-05-15 23:02:34
+ * @modify date 2021-05-15 23:02:34
+ * @desc [description]
+ */
+x = 1
+y = 2.8
+z = 1j
+
+print(type(x))
+print(type(y))
+print(type(z))
---
+++ numbers/numbers_float2.py
@@ -0,0 +1,14 @@
+/**
+ * @author Hitesh Mishra
+ * @email HiteshMishra708@gmail.com
+ * @create date 2021-05-15 23:02:01
+ * @modify date 2021-05-15 23:02:01
+ * @desc [description]
+ */
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
