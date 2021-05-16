20fd02d06b9d13ef61c5cdb1130b305b0e59d3ad
Hitesh Mishra <hiteshmishra708@gmail.com>
2021-05-16 15:45:19 +0530

    Programs for files

---
+++ files/file_open2.py
@@ -0,0 +1,10 @@
+/**
+ * @author Hitesh Mishra
+ * @email HiteshMishra708@gmail.com
+ * @create date 2021-05-16 15:44:59
+ * @modify date 2021-05-16 15:44:59
+ * @desc [description]
+ */
+f = open("demofile.txt", "r")
+
+print(f.read(5))
