d06939d92da4b077aa18c70a15021b4a07f3900b
Hitesh Mishra <hiteshmishra708@gmail.com>
2021-05-16 13:54:09 +0530

    Programs for lambda

---
+++ lambda/lambda.py
@@ -0,0 +1,9 @@
+/**
+ * @author Hitesh Mishra
+ * @email HiteshMishra708@gmail.com
+ * @create date 2021-05-16 13:53:49
+ * @modify date 2021-05-16 13:53:49
+ * @desc [description]
+ */
+x = lambda a: a + 10
+print(x(5))
---
+++ lambda/lambda2.py
@@ -0,0 +1,9 @@
+/**
+ * @author Hitesh Mishra
+ * @email HiteshMishra708@gmail.com
+ * @create date 2021-05-16 13:53:21
+ * @modify date 2021-05-16 13:53:21
+ * @desc [description]
+ */
+x = lambda a, b: a * b
+print(x(5, 6))
