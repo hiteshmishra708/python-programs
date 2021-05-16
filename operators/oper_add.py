2c929bbcbf93510fd679bc3c6a90caa5ea489ff2
Hitesh Mishra <hiteshmishra708@gmail.com>
2021-05-16 16:16:42 +0530

    Programs for operator

---
+++ operators/oper_add.py
@@ -0,0 +1,11 @@
+/**
+ * @author Hitesh Mishra
+ * @email HiteshMishra708@gmail.com
+ * @create date 2021-05-16 16:15:17
+ * @modify date 2021-05-16 16:15:17
+ * @desc [description]
+ */
+x = 5
+y = 3
+
+print(x + y)
